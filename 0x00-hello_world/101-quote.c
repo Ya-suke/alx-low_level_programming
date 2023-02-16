ee3ac5963c2ee063351fff07d76794033aeea745
ya-suke <fredluki100@gmail.com>
2023-02-16 22:48:31 +0300

    add new utility scripts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void) {
+  char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+  write(2, q, sizeof(q));
+
+  return (1);
+}
