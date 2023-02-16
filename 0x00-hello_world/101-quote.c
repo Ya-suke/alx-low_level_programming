ee0381de68b5fc9537f729004c158697fcbc9396
ya-suke <fredluki100@gmail.com>
2023-02-16 23:19:18 +0300

    add new utility scripts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,9 +10,9 @@
 
 int main(void)
 {
-	char word[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, word, sizeof(word));
+	write(2, q, sizeof(q));
 
 	return (1);
 }
