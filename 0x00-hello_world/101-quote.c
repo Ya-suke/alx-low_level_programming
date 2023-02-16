727e05cebbab0394ea3e0838e0ac1cfe0b929dae
ya-suke <fredluki100@gmail.com>
2023-02-16 22:54:39 +0300

    add new utility scripts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(void) {
-  char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+/**
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
+ */
 
-  write(2, q, sizeof(q));
-
-  return (1);
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
