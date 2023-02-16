f8b8f9dc92258c0b0f709557ac03e7d7972afcad
ya-suke <fredluki100@gmail.com>
2023-02-16 22:34:49 +0300

    add new utility scripts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * File prints and is
+ * followed by a new line
+ *
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
