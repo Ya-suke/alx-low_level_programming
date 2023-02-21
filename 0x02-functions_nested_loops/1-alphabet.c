c94917dde86773f4a173a621b4bf0a96d736c586
ya-suke <fredluki100@gmail.com>
2023-02-21 23:45:37 +0300

    add new utility scripts

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Description: prints the alphabet in lowercase, followed by a new line.
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
 void print_alphabet(void)
 {
