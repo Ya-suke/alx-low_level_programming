81b515749c5407c48be16fd43ddd50f6193143f8
ya-suke <fredluki100@gmail.com>
2023-03-07 07:21:45 +0300

    add new utility scripts

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_string - pointer to char
+ * @s: value to modify
+ * @to: value to assign
+ *
+ * Return: Nothing
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
+
